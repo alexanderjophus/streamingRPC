e553f3c8ebd5981cea320df66e53262fb75e6ea0
AJT <alextrelore@gmail.com>
2022-09-10 13:30:36 +0100

    remove unused code

--- web/src/components/People.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'; // we need this to make JSX compile
-
-type PeopleProps = {
-  name: string,
-}
-
-export const People = ({ name }: PeopleProps) => <aside>
-  <h2>{ name }</h2>
-</aside>
